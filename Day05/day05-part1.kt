72f57546901d72ac3f999ce92c799bf0d764d38c
Brenden Hyde <brendenahyde@gmail.com>
2023-12-05 13:28:07 -0800

    Continue with Day 05, part 1 in Kotlin

--- Day05/day05-part1.kt
+++ Day05/day05-part1.kt
@@ -8,9 +8,28 @@ fun readInput(filename: String): String {
     return lineList.joinToString(separator = "\n")
 }
 
-fun processMaps(puzzle: String): Int {
+fun getStartingSeeds(puzzle:String): List<Int> {
+    val startingSeeds = puzzle.split("\n\n")[0]
+    println("Handling section: \"$startingSeeds\"")
+    val spl = startingSeeds.split(": ")
+    println("After split: \"$spl\"")
+    val label = spl[0].replace(" map", "").trim()
+    println("Label: \"$label\"")
+    val values = mutableListOf<Int>()
+    spl[1]
+        .trim()
+        .split("\\s".toRegex())
+            .filter {it.length > 0 && it != ""}
+            .map {it.toInt()}
+            .map {values.add(it)}
+    println("Values: \"$values\"")
+    return values
+}
+
+fun processMaps(puzzle: String): MutableMap<String, List<List<Int>>> {
+    val allMaps = mutableMapOf<String, List<List<Int>>>()
     val sections = puzzle.split("\n\n")
-    for (section in sections) {
+    for (section in sections.slice(1..<sections.size)) {
         println("Handling section: \"$section\"")
         val spl = section.split(":")
         println("After split: \"$spl\"")
@@ -28,13 +47,33 @@ fun processMaps(puzzle: String): Int {
                 if (innerVals.size > 0 ) values.add(innerVals)
             }
         println("Values: \"$values\"")
+        allMaps[label] = values
     }
-    return 0
+    // TODO: return something real
+    return allMaps
+}
+
+fun translateViaMap(itemNumber: Int, rangeInfo: List<List<Int>>): Int {
+    for (range in rangeInfo){
+        val (destRangeStart, srcRangeStart, rangeLen) = range
+        if (itemNumber in srcRangeStart..srcRangeStart + rangeLen) {
+            return itemNumber + (destRangeStart - srcRangeStart)
+        } else {
+            continue
+        }
+    }
+    return itemNumber
 }
 
 fun main(args: Array<String>) {
     val filename = if (args.size > 0) args[0] else "sample_input.txt"
     val puzzle = readInput(filename)
     println(puzzle)
-    processMaps(puzzle)
+    // val seeds = getStartingSeeds(puzzle)
+    // println("Seeds: $seeds")
+    val allMaps = processMaps(puzzle)
+    println(allMaps)
+    // val rangeInfo = listOf(listOf(50, 98, 2), listOf(52, 50, 48))
+    // val soilNumber = translateViaMap(79, rangeInfo)
+    // println("Soil number for seed 79: $soilNumber")
 }
